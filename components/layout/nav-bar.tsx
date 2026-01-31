a014131231e2f09a17ec4f5b66e7bf2812d5ade8
Benayo <ben_odeleye@yahoo.com>
2026-01-31 09:01:07 +0100

    chore: update navbar to use Logo component

--- components/layout/nav-bar.tsx
+++ components/layout/nav-bar.tsx
@@ -2,7 +2,6 @@
 
 import { useEffect, useState } from 'react';
 import Link from 'next/link';
-import Image from 'next/image';
 
 import { Menu, X, ChevronRight } from 'lucide-react';
 import { AnimatePresence, motion } from 'framer-motion';
