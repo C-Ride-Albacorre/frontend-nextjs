0872c53cf365e1f4807bb14e4562cb4052ef6f6d
Benayo <ben_odeleye@yahoo.com>
2026-04-22 09:13:27 +0100

    refactor: enhance logging in addVendorPhoneService function

--- features/auth/services/add-vendor-phone.ts
+++ features/auth/services/add-vendor-phone.ts
@@ -3,11 +3,16 @@ import { ApiError } from '../../libs/api-error';
 import { authFetch } from '@/features/libs/auth-fetch';
 
 export async function addVendorPhoneService(data: { phoneNumber: string , countryCode: string, verificationToken?: string }) {
+
+console.log('Add vendor phone service called with:', data);
+
   const res = await fetch(`${BASE_URL}/auth/user/add/phone`, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(data),
   });
+
+
 
   const result = await res.json();
 
